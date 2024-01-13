3dc6082b6ed7d50d5e85639648daf94ce0da8c0b
muneeb usmani <muneebusmani8355@gmail.com>
2024-01-13 11:29:11 +0500

    some random changes

--- NexusCommunication/Program.cs
+++ NexusCommunication/Program.cs
@@ -5,8 +5,7 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-
-        // Add services to the container.
+        // Add services to the container. 
         builder.Services.AddControllersWithViews();
 
         var app = builder.Build();
